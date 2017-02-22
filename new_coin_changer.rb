5aa3dd33b33969fdfbe4c271b66cc3c7171f5683
Spac3Man2 <Spac3Man2>
2017-02-22 14:59:28 -0500

    new_coin_changer

--- new_coin_changer.rb
+++ new_coin_changer.rb
@@ -1,4 +1,14 @@
 def new_coin_changer(amount)
 	# There is no change to give. The empty hash will represent that.
-	change = {}
+	  
+   change = {}  
+    
+    if amount == 0
+
+	elsif amount < 5 
+
+		change = {penny: amount}
+
+    end 
+    change 
 end
--- test_new_coin_changer.rb
+++ test_new_coin_changer.rb
@@ -5,8 +5,14 @@ class TestNewCoinChanger < Minitest::Test
 
     def test_for_cents_returns_empty_hash
     # Runs a test to see if the test returns zero cents
-       total = new_coin_changer(0)
-       assert_equal({}, total)
+       results = new_coin_changer(0)
+       assert_equal({}, results)
+    end
+
+    def test_for_3_pennies_returns_3_cents
+     
+     results = new_coin_changer(3)
+     assert_equal({penny: 3}, results)
     end
 
 end       	
