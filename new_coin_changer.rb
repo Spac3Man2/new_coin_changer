16915d5313e13d6072ab3bd9285cd09a0c6cfe0b
Spac3Man2 <Spac3Man2>
2017-02-22 14:05:59 -0500

    new_coin_changer

--- new_coin_changer.rb
+++ new_coin_changer.rb
@@ -1,4 +1,4 @@
 def new_coin_changer(amount)
-	
+	# There is no change to give. The empty hash will represent that.
 	change = {}
 end
--- test_new_coin_changer.rb
+++ test_new_coin_changer.rb
@@ -4,6 +4,7 @@ require_relative 'new_coin_changer.rb'
 class TestNewCoinChanger < Minitest::Test
 
     def test_for_cents_returns_empty_hash
+    # Runs a test to see if the test returns zero cents
        total = new_coin_changer(0)
        assert_equal({}, total)
     end
